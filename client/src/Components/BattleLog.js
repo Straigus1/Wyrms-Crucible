9bfcb841dfd8be25a90286e9c0087a1bb31d5ab1
Dayton <kirylmitch@gmail.com>
2022-02-08 11:28:58 -0500

    added battle log

--- client/src/Components/BattleLog.js
+++ client/src/Components/BattleLog.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import TypeWriterEffect from 'react-typewriter-effect';
+import battlelog from '../Images/battle-log.png'
 
-function BattleLog() {
+function BattleLog({setRogueHealth, setPaladinHealth, setSorcererHealth, setEnemyHealth, enemyAttack, rogueHealth, sorcererHealth, paladinHealth, enemyHealth}) {
+    
+  
+    function logActions () {
+        let damage = enemyAttack
+        if (rogueHealth--) {
+            return <p>Rogue took {damage} damage!</p>
+        } else if (paladinHealth--) {
+            return <p>Paladin took {damage} damage!</p>
+        } else if (sorcererHealth--) {
+            return <p>Sorcerer took {damage} damage!</p>
+        }
+    }
+       
 
     return (
-        <div className='battle-log'>
-            
+        <div className='battle-log'> 
+            <img 
+            className='log-image'
+            src={battlelog}
+            alt='battle log'
+            />
+            {logActions()}
         </div>
     )
 
